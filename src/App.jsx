79f2fb55eb5a12365bbaeda6eed1f0705ab42027
nael <jnaelchoquepuma77@gmail.com>
2023-02-21 00:28:17 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css'
+import QuoteBox from './components/QuoteBox';
+import db from './db/quotes.json'
+
+const arrayColors = ["#ff6961", "#77dd77", "#fdfd96", "#84b6f4", "#fdcae1"]
+
+function App() {
+  const getRandom =(arrayElements) =>{
+    const quantityValues = arrayElements.length;
+    const randomIndex = Math.floor(Math.random()*quantityValues); 
+
+    return arrayElements[randomIndex];
+  };
+
+  const [quote, setQuote] = useState (getRandom(db));
+  const [color, setColor] = useState(getRandom(arrayColors));
+
+  const newQuote = () => {
+    setQuote(getRandom(db));
+    setColor(getRandom(arrayColors))
+  };
+
+  return (
+    <div className="App" style={{backgroundColor:color}}>
+      <div>
+       <QuoteBox 
+       quote = {quote} 
+       newQuote = {newQuote} 
+       color = {color}
+       /> 
+      </div>
+    </div>
+  );
+}
+
+export default App
